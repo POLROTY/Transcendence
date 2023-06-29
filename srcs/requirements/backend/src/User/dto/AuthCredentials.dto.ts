114afd55e3feb33e1eb1176180bb9b03d9875649
aptive <thibaut1515@hotmail.fr>
2023-06-29 17:37:40 +0200

    Revert "merge avec front"
    This reverts commit 860d6e3317b3baef63214c60c2040a25ea1cd5c4.

---
+++ srcs/requirements/backend/src/User/dto/AuthCredentials.dto.ts
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AuthCredentials.dto.ts                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aptive <aptive@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/29 02:39:21 by aptive            #+#    #+#             */
+/*   Updated: 2023/06/29 03:44:59 by aptive           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+import { ApiProperty } from "@nestjs/swagger";
+import {IsEmail, IsNumber, IsString }from '@nestjs/class-validator';
+
+export class AuthCredentialsDto {
+
+	@ApiProperty({
+		description: 'Unique ID autoincremente',
+	})
+	@IsNumber()
+	readonly id : number;
+
+	@ApiProperty({
+		description: 'Unique Email',
+	})
+	@IsEmail()
+	readonly email : string;
+
+	@ApiProperty({
+		description: 'name User',
+	})
+	@IsString()
+	readonly name? : string;
+
+	@ApiProperty({
+		description: 'name User',
+	})
+	@IsString()
+	password : string;
+}
