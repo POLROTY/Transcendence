c4b3eae462ce60b9ddf2d0971b985ed74165755b
Choums <chams.aidel@outlook.fr>
2023-06-30 18:06:41 +0200

    whichHeaderRoute a finir

--- srcs/requirements/frontend/src/app/components/header/header.component.ts
+++ srcs/requirements/frontend/src/app/components/header/header.component.ts
@@ -1,17 +1,33 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+enum HeaderDisplay {
+  Main = 1,
+  Game = 2,
+  Friends = 3,
+  Profile = 4,
+  Settings = 5,
+  Login = 0,
+}
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  
+  constructor(private router: Router, private headerDisplay: HeaderDisplay) {
 
-  constructor(private router: Router) {}
-
-  isFriend(route: string): boolean {
-    return (this.router.url === route);
   }
+  
+  // whichHeaderRoute(): number {
+  //   let route: string = this.router.url;
+  //   console.log(route);
+  //   // route = route.slice();
+  //   // switch ()
+    
+  //   return 3;
+  // }
 
 }
